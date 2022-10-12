ffa0166f761b73ecb5349fdaba4481995a9f912c
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-12 15:42:40 -0600

    header and nav

--- assets/JS/animalkingdom.js
+++ assets/JS/animalkingdom.js
@@ -1,11 +1,8 @@
 
-let center = [4, 44.4]
-const map = tt.map({
-    key: 'IGkKkr9lTGqbUJAvV4QPUVTyT1ACs4XG',
-    container: 'map',
-    center: center,
-    zoom: 10
-})
-map.on('load', () =>  {
-    new tt.Marker().setLngLat("28.35806, -81.59000").addTo(map)
-})      
+var map = tt.map({ 
+    key: 'IGkKkr9lTGqbUJAvV4QPUVTyT1ACs4XG', 
+    container: 'map', 
+    center: [-81.590600, 28.3576], 
+    zoom: 15 
+});
+
