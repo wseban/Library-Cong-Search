aec1a51bc41b31a284afce6cafeb21390eaca5d9
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-13 13:01:33 -0600

    added search bar functionally running api and local storage

--- assets/JS/searchpage.js
+++ assets/JS/searchpage.js
@@ -1,4 +1,14 @@
-var themeParkUrl = "https://api.disneyapi.dev/character?name=Mickey%20Mouse"
+function saveInput () {
+    var input = document.getElementById('search-bar').value
+    var inputSplit = input.split(" ");
+    var charName = inputSplit.join("%20")
+    console.log(charName)
+    localStorage.setItem("recent-search", charName)
+
+
+
+
+    var themeParkUrl = "https://api.disneyapi.dev/character?name="  + charName;
 var themeParkUrl2 = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=4geqQK1CQGBP4icGVsqsLn9aqaMb0cjhUXs79A9V&search_field=name&search_value=mickey%20mouse&search_type=1"
 
 
@@ -12,4 +22,8 @@ fetch (themeParkUrl, {
     console.log(data)
 }
 )
+
+}
+
+
 //linked APIs up top
