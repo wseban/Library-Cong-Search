339ee7c6cca25fc1795a64c90fe013e221ce0904
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-13 15:29:02 -0600

    working api

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,3 @@
-// let center = [4, 44.4]
-// const map = tt.map({
-//     key: 'IGkKkr9lTGqbUJAvV4QPUVTyT1ACs4XG',
-//     container: 'map',
-//     center: center,
-//     zoom: 10
-// })
-// map.on('load', () =>  {
-//     new tt.Marker().setLngLat(center).addTo(map)
-// })      
 
 //Slideshow Functionality 
 var slideIndex = 0;
