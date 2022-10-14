a139eabf9d12cd0098d956a2befcc3ed7b53a2fd
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-10-14 15:33:48 -0600

    working Omdb second API functional

--- assets/JS/searchpage.js
+++ assets/JS/searchpage.js
@@ -1,4 +1,3 @@
-
 // function that gets called when user inputs their favortie character into the search bar
 
 var displayCharName = document.getElementById('char-name')
@@ -8,9 +7,18 @@ var displayCharImage = document.getElementById('char-image')
 var displayStreamedMovies = document.getElementById('streamed-movies')
 var displayStreamedShows = document.getElementById('streamed-shows')
 
+var displayTitle = document.getElementById('title')
+var displayPoster = document.getElementById('poster')
+var posterImage = document.getElementById('poster-img')
+var displayReleaseDate = document.getElementById('release')
+var displayRating = document.getElementById('rating')
+var displayPlot = document.getElementById('plot')
 
 
 
+
+
+// --------------------------------------  EVENT LISTENERS  ---------------------- ENTER AND CLICK  -------------------
 
 var inputClick = document.getElementById('search-button')
 inputClick.addEventListener("click", renderInput);
@@ -27,7 +35,11 @@ inputEnter.addEventListener("keypress", function(event) {
 });
 
 
-function renderInput () {
+// --------------------------------  FORMATTING USER INPUT  ---------------------------------------
+
+function renderInput () { // Function that is called on click and Enter
+
+    // Iterates through the inputed character name and Uppercases the First Letter and lower cases the rest + adds the required "%20" between spaces so that the API can be called properly - 
     var input = document.getElementById('search-bar').value
     var words = input.split(" ");
    
@@ -37,20 +49,22 @@ function renderInput () {
 
     var charName = words.join("%20")
 
+//--------------------  DISNEY API  -----------------------------------------------
 var getTitles = "https://api.disneyapi.dev/character?name=" + charName;
 
 fetch(getTitles)
 .then(response => response.json())
 .then(data => {
     
-    var movies = data['data'][0]['films']
-    var shows = data['data'][0]['tvShows']
-    var charName = data['data'][0]['name']
-    var image = data['data'][0]['imageUrl']
+    var movies = data['data'][0]['films'] // Retrieves the data (Films) stores it in the variable
+    var shows = data['data'][0]['tvShows'] // Retrieves the data (TV Shows) stores it in the variable
+    var charName = data['data'][0]['name'] // Retrieves the data (Character Name) stores it in the variable
+    var image = data['data'][0]['imageUrl'] // Retrieves the data (Image of Character) stores it in the variable
 
-    displayCharName.innerHTML = charName;
-    displayCharImage.setAttribute("src", image);
+    displayCharName.innerHTML = charName;  // Displays Character name on screen
+    displayCharImage.setAttribute("src", image); // Displays Character Image on screen
 
+    //Clears the text content of the list items after and before each search ---
     if(displayShowTitles.getElementsByTagName("ul")){
         displayShowTitles.textContent = ""
     }
@@ -58,11 +72,15 @@ fetch(getTitles)
         displayMovieTitles.textContent = ""
     }
     
+    // Appends 2 Unordered Lists for Movies & Shows
     var showList = document.createElement("ul")
     displayShowTitles.append(showList)
     var movieList = document.createElement("ul")
     displayMovieTitles.append(movieList)
 
+
+// -------------------------   SHOWS  -----------------------------   
+    // Appends ALL List items (As much data that is availible) to be contained within the UL of Show Titles
     for(var i = 0; i < shows.length; i++){
         var showListItem = document.createElement("li")
         var showLink = document.createElement("a")
@@ -70,47 +88,88 @@ fetch(getTitles)
         showList.appendChild(showListItem)
         showListItem.appendChild(showLink)
         showLink.setAttribute("id", i)
-        //var listenLink = document.getElementById(i);
-        //listenLink.addEventListener("click", renderPlatforms);
-
+        var listenShowLink = document.getElementById(i);
+        listenShowLink.addEventListener("click", renderInfo);     
     }
-
-    for(var i = 0; i < movies.length; i++){
+    
+// -------------------------   MOVIES  -----------------------------
+    // Appends ALL List items (As much data that is availible) to be contained within the UL of Movie Titles
+    for(var j = 0; j < movies.length; j++){
+        var ten = 100;
         var movieListItem = document.createElement("li")
         var movieLink = document.createElement("a")
-        movieLink.textContent = movies[i]
+        movieLink.textContent = movies[j]
         movieList.appendChild(movieListItem)
         movieListItem.appendChild(movieLink)
+        movieLink.setAttribute("id", ten + j);
+        var listenMovieLink = document.getElementById(ten + j); 
+        listenMovieLink.addEventListener("click", renderInfo);
     }
+    
 }) 
 
 }
 
 
-// http://www.omdbapi.com/?i=tt3896198&apikey=61416bd7
+
+// --------------------------------  OMDB API  -------------------------------------
 
 
-function renderPlatforms() {
+function renderInfo(event) {
+    var userValue = event.target.innerText;
+
+    var splitUserValue = userValue.split(" ");
    
-    fetch('http://www.omdbapi.com/?t=The%20Mickey%20Mouse%20Club&apikey=61416bd7')
+    for (var i = 0; i < splitUserValue.length; i++) {
+        splitUserValue[i] = splitUserValue[i][0].toUpperCase() + splitUserValue[i].substr(1);
+    }
+
+    var joinUserValue = splitUserValue.join("%20")
+
+    console.log(joinUserValue);
+
+
+      fetch('http://www.omdbapi.com/?t=' + joinUserValue + '&apikey=61416bd7')
         .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+        .then(data => {
+
+        console.log(data)
+        
+        var title = data['Title'];
+        var releaseDate = data['Released'];
+        var rating = data['imdbRating'];
+        var plot = data['Plot'];
+        
+        console.log(plot);
+        console.log(rating);
+        var poster = data['Poster'];
+        posterImage.setAttribute("src", poster); 
+        
+        
+        
+
+        displayTitle.innerHTML = title;
+        displayReleaseDate.innerHTML = releaseDate;
+        displayRating.innerHTML = rating;
+        displayPlot.innerHTML = plot;
+        displayPoster.innerHTML = poster; 
+        })
+
+
 }
 
 
 
-function listShowLinks(event){
-    //
-    console.log(event)
-}
-function listMovieLinks(){
-    movieLink.setAttribute("onClick", renderPlatforms())
-
-}
 
 
 
+
+
+
+
+
+
+/*  // Anime - functionality 
 var button = document.getElementById('search-button');
 
 mouseHoverAnimation = () => {
@@ -125,4 +184,4 @@ mouseHoverAnimation = () => {
     })
 }
 
-button.addEventListener('mouseover', mouseHoverAnimation)
+button.addEventListener('mouseover', mouseHoverAnimation)  */
